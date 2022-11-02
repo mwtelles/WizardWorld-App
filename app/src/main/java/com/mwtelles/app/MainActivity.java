f4cb54145babf47bfd3ff62d5bc809190a86ee1f
Matheus Telles <business.mwtelles@gmail.com>
2022-11-02 19:07:29 -0300

    feat: animando a navegação

--- app/src/main/java/com/mwtelles/app/MainActivity.java
+++ app/src/main/java/com/mwtelles/app/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -52,6 +54,13 @@ public class MainActivity extends AppCompatActivity {
                     casasText.setVisibility(View.VISIBLE);
                     casasImage.setImageResource(R.drawable.casas_selected_icon);
                     casasLayout.setBackgroundResource(R.drawable.round_back_home_100);
+
+                    ScaleAnimation scaleAnimation = new ScaleAnimation(0.8f, 1.0f, 1f, 1f, Animation.RELATIVE_TO_SELF, 0.0f, Animation.RELATIVE_TO_SELF, 0.0f)
+                    scaleAnimation.setDuration(200);
+                    scaleAnimation.setFillAfter(true);
+
+                    casasLayout.startAnimation(scaleAnimation);
+                    selectedTab = 1;
                 }
             }
         });
@@ -60,6 +69,30 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                if(selectedTab != 2) {
+                    casasText.setVisibility(View.GONE);
+                    feiticosText.setVisibility(View.GONE);
+                    bruxosText.setVisibility(View.GONE);
+
+                    casasImage.setImageResource(R.drawable.casas_icon);
+                    feiticosImage.setImageResource(R.drawable.feiticos_icon);
+                    bruxosImage.setImageResource(R.drawable.bruxos_icon);
+
+                    casasLayout.setBackgroundColor(getResources().getColor(android.R.color.transparent));
+                    feiticosLayout.setBackgroundColor(getResources().getColor(android.R.color.transparent));
+                    bruxosLayout.setBackgroundColor(getResources().getColor(android.R.color.transparent));
+
+                    pocoesText.setVisibility(View.VISIBLE);
+                    pocoesImage.setImageResource(R.drawable.pocoes_selected_icon);
+                    pocoesLayout.setBackgroundResource(R.drawable.round_back_pocoes_100);
+
+                    ScaleAnimation scaleAnimation = new ScaleAnimation(0.8f, 1.0f, 1f, 1f, Animation.RELATIVE_TO_SELF, 1.0f, Animation.RELATIVE_TO_SELF, 0.0f)
+                    scaleAnimation.setDuration(200);
+                    scaleAnimation.setFillAfter(true);
+
+                    pocoesLayout.startAnimation(scaleAnimation);
+                    selectedTab = 2;
+                }
             }
         });
 
@@ -67,12 +100,62 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                if(selectedTab != 3) {
+                    casasText.setVisibility(View.GONE);
+                    pocoesText.setVisibility(View.GONE);
+                    bruxosText.setVisibility(View.GONE);
+
+                    casasImage.setImageResource(R.drawable.casas_icon);
+                    pocoesImage.setImageResource(R.drawable.pocoes_icon);
+                    bruxosImage.setImageResource(R.drawable.bruxos_icon);
+
+                    casasLayout.setBackgroundColor(getResources().getColor(android.R.color.transparent));
+                    pocoesLayout.setBackgroundColor(getResources().getColor(android.R.color.transparent));
+                    bruxosLayout.setBackgroundColor(getResources().getColor(android.R.color.transparent));
+
+                    feiticosText.setVisibility(View.VISIBLE);
+                    feiticosImage.setImageResource(R.drawable.feiticos_selected_icon);
+                    feiticosLayout.setBackgroundResource(R.drawable.round_back_feiticos_100);
+
+                    ScaleAnimation scaleAnimation = new ScaleAnimation(0.8f, 1.0f, 1f, 1f, Animation.RELATIVE_TO_SELF, 1.0f, Animation.RELATIVE_TO_SELF, 0.0f)
+                    scaleAnimation.setDuration(200);
+                    scaleAnimation.setFillAfter(true);
+
+                    feiticosLayout.startAnimation(scaleAnimation);
+                    selectedTab = 3;
+                }
+
             }
         });
 
         bruxosLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                if(selectedTab != 4) {
+                    casasText.setVisibility(View.GONE);
+                    pocoesText.setVisibility(View.GONE);
+                    feiticosText.setVisibility(View.GONE);
+
+                    casasImage.setImageResource(R.drawable.casas_icon);
+                    pocoesImage.setImageResource(R.drawable.pocoes_icon);
+                    feiticosImage.setImageResource(R.drawable.feiticos_icon);
+
+                    casasLayout.setBackgroundColor(getResources().getColor(android.R.color.transparent));
+                    pocoesLayout.setBackgroundColor(getResources().getColor(android.R.color.transparent));
+                    feiticosLayout.setBackgroundColor(getResources().getColor(android.R.color.transparent));
+
+                    bruxosText.setVisibility(View.VISIBLE);
+                    bruxosImage.setImageResource(R.drawable.bruxos_selected_icon);
+                    bruxosLayout.setBackgroundResource(R.drawable.round_back_bruxos_100);
+
+                    ScaleAnimation scaleAnimation = new ScaleAnimation(0.8f, 1.0f, 1f, 1f, Animation.RELATIVE_TO_SELF, 1.0f, Animation.RELATIVE_TO_SELF, 0.0f)
+                    scaleAnimation.setDuration(200);
+                    scaleAnimation.setFillAfter(true);
+
+                    bruxosLayout.startAnimation(scaleAnimation);
+                    selectedTab = 4;
+                }
 
             }
         });
